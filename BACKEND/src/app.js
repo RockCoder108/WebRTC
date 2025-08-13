a6c2823403d5d910b52e8efb76003aea2a1fe024
RockCoder108 <samyak80jan@gmail.com>
2025-08-14 04:20:19 +0530

    first commit

---
+++ BACKEND/src/app.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+import { log } from 'node:console';
+import { connectToSocket } from './controllers/socketManager.js';
+
+
+
+
+const app = express();
+const server = createServer(app);
+const io =  connectToSocket(server);
+
+app.set("port", process.env.PORT || 8000);
+app.use(cors());
+app.use(express.json({limit: '40kb'}));
+app.use(express.urlencoded({extended: true, limit: '40kb'}));
+
+
+
+const startServer = async () => {
+    const connectionDb = await mongoose.connect(
+      "mongodb+srv://jain105samyak:4GPnHiNH0EPFVB8I@cluster0.z5vwvbn.mongodb.net/"
+    );
+    console.log(`Connected to MongoDB at ${connectionDb.connection.host}`);
+    server.listen(app.get("port"), () => {
+        console.log("Server is running on port 8000");
+    })
+}
+
+startServer();
