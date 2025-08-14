3a30867f129c067d23403c0384576b5e7b03d23c
RockCoder108 <samyak80jan@gmail.com>
2025-08-14 05:32:20 +0530

    Implement user authentication with login and registration endpoints

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -6,6 +6,7 @@ import { Server } from 'socket.io';
 import cors from 'cors';
 import { log } from 'node:console';
 import { connectToSocket } from './controllers/socketManager.js';
+import userRoutes from "./routes/users.routes.js"
 
 
 
@@ -18,6 +19,9 @@ app.set("port", process.env.PORT || 8000);
 app.use(cors());
 app.use(express.json({limit: '40kb'}));
 app.use(express.urlencoded({extended: true, limit: '40kb'}));
+
+app.use("/api/v1/users", userRoutes);
+
 
 
 
